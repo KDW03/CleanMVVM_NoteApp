529729d3926a7d84fc464b3fa585c2566c01fb97
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-07-08 21:54:19 +0900

    feat : hilt moudle 정의 (database, repository, usecases)

--- app/src/main/java/com/example/noteapp/NoteApp.kt
+++ app/src/main/java/com/example/noteapp/NoteApp.kt
@@ -1,4 +1,26 @@
 package com.example.noteapp
 
-class NoteApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * @HiltAndroidApp 어노테이션은 Hilt가 코드를 생성하게 하여 앱이 의존성 주입을 가능하게 하는 트리거 역할
+ * 이 어노테이션은 애플리케이션 레벨의 의존성 컨테이너 역할을 하는 Base Application을 만들고 Hilt에게 코드 생성을 하도록 트리거를 만듬
+ *
+ *
+ * 따라서, @HiltAndroidApp 어노테이션이 붙은 클래스는 앱의 수명주기와 밀접하게 연결되며,
+ * Hilt를 사용하고자 하는 모든 앱은 @HiltAndroidApp 어노테이션을 포함한 Application 클래스를 가지고 있어야 한다
+ *
+ * Hilt는 안드로이드에서 의존성 주입을 도와주는 라이브러리로 수명주기를 자동으로 관리해준다
+ * Hilt는 Dagger 라이브러리르 기반, Jetpack에서 권장하는 DI 라이브러리
+ *
+ *
+ * 의존성 주입은 객체 간의 의존 관계를 객체 자신이 아닌 외부에서 설정하는 것을 의미
+ *이를 통해 코드 재사용성, 리팩터링 편의성, 테스트 편의성 등의 이점
+ *
+ * Hilt는 프로젝트에서 수동 DI를 사용하는 상용구 코드를 줄여 줌
+ *
+ */
+@HiltAndroidApp
+class NoteApp : Application()
---
+++ app/src/main/java/com/example/noteapp/feature_note/di/AppModule.kt
@@ -0,0 +1,70 @@
+package com.example.noteapp.feature_note.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.noteapp.feature_note.data.data_source.NoteDatabase
+import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.example.noteapp.feature_note.domain.repository.NoteRepository
+import com.example.noteapp.feature_note.domain.use_case.DeleteNote
+import com.example.noteapp.feature_note.domain.use_case.GetNotes
+import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+/**
+ * @Module 어노테이션은 이 클래스가 Hilt 모듈임을 나타낸다.
+ * 모듈에서 정의된 의존성들은 Hilt가 의존성 그래프를 구축하는 데 사용
+ *
+ *
+ * @InstallIn 어노테이션은 이 모듈이 어떤 Hilt 컴포넌트에 설치될지 지정, 이 경우 SingletonComponent에 설치
+ * Hilt 모듈은 의존성 주입을 위한 객체를 제공하는 클래스이다
+ * SingletonComponent는 애플리케이션 수명주기와 연결된 컴포넌트로, 애플리케이션 전체에서 단일 인스턴스만 생성되는 종속성을 제공
+ *
+ *
+ * 세 가지 종속성을 제공
+ *
+ * @Provides 어노테이션은 이 어노테이션은 이 함수가 의존성을 제공하는 함수임을 나타냄
+ * @Singleton 어노테이션은 단일 인스턴스만 생성한다는 뜻
+ *
+ *
+ * Application 객체는 Hilt가 자동으로 제공하는 기본 바인딩 중 하나
+ * @HiltAndroidApp 어노테이션이 붙은 클래스의 인스턴스가 Application 객체로 사용
+ * 따라서, 별도로 Application 객체를 제공하는 @Provides 함수를 작성하지 않아도, Hilt가 자동으로 Application 객체를 주입
+ *
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+
+}
