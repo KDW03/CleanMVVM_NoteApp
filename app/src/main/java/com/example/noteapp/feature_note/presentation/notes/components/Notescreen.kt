659234f4b0ebed5eb65a7bc6c33cf523e8091d62
kimdowoo <asasa0628@gs.cwnu.ac.kr>
2023-07-10 14:26:58 +0900

    Notescreen 구현

--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/Notescreen.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/Notescreen.kt
@@ -1,2 +1,159 @@
 package com.example.noteapp.feature_note.presentation.notes.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Sort
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.noteapp.feature_note.presentation.notes.NotesEvent
+import com.example.noteapp.feature_note.presentation.notes.NotesViewModel
+import com.example.noteapp.ui.theme.NoteAppTheme
+import kotlinx.coroutines.launch
+
+
+/**
+ *  노트 앱의 화면을 구성하는 NoteScreen 컴포저블
+ *
+ *  Scaffold는 레이아웃 컴포저블 중 하나로, 기보적인 앱 구조를 제공한다.
+ *  Scaffold 컴포저블은 상단 바, 하단 바, 드로어, 플로팅 액션 버튼, 스낵바 등의 요소를 포함
+ *  Scaffold를 사용하면 쉽게 앱의 레이아웃을 구성
+ *  파라미터
+ *
+ *  topBar : 상단 바를 정의하는 컴포저블
+ *  bottomBar : 하단 바 정의
+ *  drawerContent : 드로어 내용을 정의하는 컴포저블
+ *  floatingActionButton : 플로팅 액션 버튼 정의
+ *  snackbarHost: 스냅가를 표시하는 호스트
+ *  content : 앱의 주요 내용을 정의하는 컴포저블
+ *
+ *
+ *
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteScreen(
+    navController: NavController? = null,
+    viewModel: NotesViewModel
+) {
+    val state = viewModel.state.value
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                containerColor = MaterialTheme.colorScheme.primary
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
+            }
+        },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(it)
+        ) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Your note",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
+            IconButton(onClick = { viewModel.onEvent(NotesEvent.ToggleOrderSection) }) {
+                Icon(imageVector = Icons.Default.Sort, contentDescription = "Sort")
+            }
+
+
+            AnimatedVisibility(
+                visible = state.isOrderSectionVisible,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()
+            ) {
+                OrderSection(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 16.dp),
+                    noteOrder = state.noteOrder,
+                    onOrderChange = {
+                        viewModel.onEvent(NotesEvent.Order(it))
+                    })
+
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.notes) { note ->
+                    NoteItem(
+                        note = note, modifier = Modifier
+                            .fillMaxSize()
+                            .clickable {
+
+                            },
+                        onDeleteClick = {
+                            viewModel.onEvent(NotesEvent.DeleteNote(note))
+                            scope.launch {
+
+                                val result = snackbarHostState.showSnackbar(
+                                    message = "Note deleted",
+                                    actionLabel = "Undo"
+                                )
+
+                                if (result == SnackbarResult.ActionPerformed) {
+                                    viewModel.onEvent(NotesEvent.RestoreNote)
+                                }
+
+                            }
+                        }
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+}
+
+
+
+
